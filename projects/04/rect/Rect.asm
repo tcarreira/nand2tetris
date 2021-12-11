a0aa6957bd5a105052cb5b018d79050752765536
fabexdias <fabiomagdias@hotmail.com>
2021-12-11 00:25:19 +0000

    Added Rect project

---
+++ projects/04/rect/Rect.asm
@@ -0,0 +1,9 @@
+// This file is inspired on the word done in www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Fábio Dias, IST.
+// File name: projects/04/Rect.asm
+
+// Draws a rectangle on the right half of the screen
+// with 16 pixels of length and RAM[0]-1 pixels height
+
+// Put your code here.
