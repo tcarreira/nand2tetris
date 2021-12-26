fab6dc62add5143a98be3cc7fc3d9bee544ffcc5
fabexdias <fabiomagdias@hotmail.com>
2021-12-26 15:05:24 +0000

    Added Adder subproject (project-04)

---
+++ projects/04/adder/Adder.asm
@@ -0,0 +1,20 @@
+// This file is inspired on the work done in www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Daniel Pacheco, IST.
+// File name: projects/04/adder/Adder.asm
+
+// This programs takes two one digit numbers as input and calculates their sum,
+// putting the result in RAM[0].
+// In order to do so, the program should be divided into three sections:
+// - Waits for the first number to be pressed and stores it;
+// - Waits until nothing is being pressed (to allow the second number to be pressed afterwards);
+// - Waits for the second number to be pressed, adds it to the first number and places
+// the result in RAM[0]
+// Because on each of these sections we have to wait for user input, you should use
+// a loop for each section.
+// Reminder: We are not adding the key code of each number we press, we are adding the
+// value of said number, therefore there needs to be a conversion from key code to
+// number value.
+
+
+// Put your code here
