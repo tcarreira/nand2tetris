17a099a9d9b848e28f23d9163ced27720959ddbc
Rafael Jerónimo <rafael.jer@gmail.com>
2021-12-27 12:50:48 +0000

    Add NumbersToMemory and NumbersPressed (project-04)

---
+++ projects/04/numberstomemory/NumbersToMemory.asm
@@ -0,0 +1,4 @@
+// Program: NumbersToMemory.asm
+// Description: Press a key of a number at some moment and stores that number at RAM[0] 
+//      until other key is pressed (don't validate if input is a number)
+// Usage: Press a key of a number and check the value at RAM[0]
